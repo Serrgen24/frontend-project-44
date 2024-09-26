a486efab583dc0c51535a7052c80d5001967a836
Sergey Kiryanov <SKiryanov@slb.com>
2024-09-26 15:20:55 +0700

    3 step: add readline-sync, add cli.js

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync'
+
+// Познакомимся и поприветствуем 
+const userName = () => {
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+};
+export default userName;
