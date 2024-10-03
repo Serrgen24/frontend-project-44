d9dd5a832f34173b1a5343ca3ba0bd8661f63737
Sergey Kiryanov <SKiryanov@slb.com>
2024-10-03 11:51:02 +0700

    add game gcd

---
+++ src/games/gcd.js
@@ -0,0 +1,40 @@
+/* eslint-disable linebreak-style */
+import readlineSync from 'readline-sync';
+import getRandomNumber from 'getrandomnumber';
+
+import { name } from '../index.js';
+
+let gameCount = 0;
+
+const gcd = () => {
+  console.log('Find the greatest common divisor of given numbers.');
+  const getRandNum = () => getRandomNumber(1, 20);
+  const x = getRandNum();
+  const y = getRandNum();
+  const result = `${x} ${y}`;
+
+  console.log(`Question: ${result}`);
+  const userAnswer = Number(readlineSync.question('Your answer: '));
+
+  const arr = [];
+  for (let i = 1; i <= Math.min(x, y); i += 1) {
+    if (x % i === 0 && y % i === 0) {
+      arr.push(i);
+    }
+  }
+  const expectedAnswer = arr.at(-1);
+
+  if (expectedAnswer === userAnswer) {
+    console.log('Correct!');
+    gameCount += 1;
+    if (gameCount !== 3) {
+      gcd();
+    } else {
+      console.log(`Congratulations, ${name}!`);
+    }
+  } else {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'. \nLet's try again, ${name}!`);
+  }
+};
+
+export default gcd;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { name } from './utils/userName.js';
 import even from './games/even.js';
 import calc from './games/calc.js';
+import gcd from './games/gcd.js';
 
 console.log('Welcome to the Brain Games!');
 console.log(`Hello, ${name}!`);
 even();
 calc();
+gcd();
 
 export { name };
