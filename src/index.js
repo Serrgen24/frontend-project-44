9bca44b221e5415f8dee12a7b6df4fc2b9ffcf3d
Serrgen <serrgen@yandex.ru>
2024-10-19 17:08:28 +0700

    fix index.js

--- src/index.js
+++ src/index.js
@@ -9,16 +9,23 @@ const startGame = (gameRule, getGameParams) => {
   console.log(`Hello, ${userName}!`);
   console.log(gameRule);
 
-  for (let i = 0; i < gameCount; i += 1) {
+  for (let i = 1; i <= gameCount; i += 1) {
     const [userQuestion, expectedAnswer] = getGameParams();
     console.log(`Question: ${userQuestion}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    console.log('Correct!');
+
+    if (userAnswer === expectedAnswer) {
+      console.log('Correct!');
+    }
 
     if (userAnswer !== expectedAnswer) {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'.`);
       console.log(`Let's try again, ${userName}!`);
       return;
+    }
+
+    if (gameCount === i) {
+      console.log(`Congratulations, ${userName}`);
     }
   }
 };
